a815e78d00f25bf7cdb4916fe86d229a2dda3288
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-01-17 16:25:39 +0900

    Add files via upload

--- page-service.php
+++ page-service.php
@@ -171,23 +171,5 @@ Flow
     </section>
 </div>
 
-<!--==============================================================
-contact
-============================================================== -->
-<div class="Contact">
-    <div class="section__title fadein">
-        <h2>Contact</h2>
-        <p>お問い合わせ</p>
-    </div>
-
-    <p>お仕事のご依頼やご相談はこちら<br>
-        お気軽にご連絡ください！
-    </p>
-<div class="wrapper">
-    <div class="button__more ">
-        <a href="<?php echo esc_url(home_url('/contact/')); ?>">お問い合わせ</a>
-    </div>
-</div>
-</div>
-
+<?php get_template_part('contact-part'); ?>
 <?php get_footer(); ?>
