1096a5c0251587071b15097870ffa0ae669bae9d
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-01-22 06:51:21 +0900

    Add files via upload

--- page-contact.php
+++ page-contact.php
@@ -3,6 +3,7 @@
 Template Name: contact
 */
 get_header('contact'); ?>
+
 <!--==============================================================
 ContactPage
 ============================================================== -->
@@ -11,35 +12,17 @@ ContactPage
         <div class="section__title fadein">
             <h2>Contact</h2>
             <p>お問い合わせ</p>
+            <p class="caution">以下の内容で送信します。よろしいですか？</p>
+            <?php if( is_page('contact')):?>
+                <script type="text/javascript">
+                    if($('.mw_wp_form_confirm')) {
+                        $('.caution').css('display', 'block');
+                    }
+                </script>
+                <?php endif;?>
         </div>
-        <div class="Contact__form">
-            <ul>
-                <li>
-                    <label for="name01">お名前<span class="must">必須</span></label>
-                    <input type="text" name="your-name01" id="name01">
-                </li>
-                <li>
-                    <label for="name02">ふりがな<span class="must">必須</span></label>
-                    <input type="text" name="your-name02" id="name02">
-                </li>
-                <li>
-                    <label for="number">電話番号</label>
-                    <input type="tel" name="your-number" id="number">
-                </li>
-                <li>
-                    <label for="address">メールアドレス<span class="must">必須</span></label>
-                    <input type="email" name="your-address" id="address">
-                </li>
-                <li>
-                    <label for="message">お問い合わせ内容<span class="must">必須</span></label>
-                    <textarea name="message" id="message" cols="30" rows="10"></textarea>
-                </li>
-            </ul>
-
-            <div class="submit__btn">
-                <input type="submit" value="確認画面へ" class="submit">
-            </div>
-        </div>
+        <?php the_content(); ?>
+        
     </div>
 </section>
 
